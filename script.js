43f1cb3cc0051008806c4df49d508682e97254e9
abrusc93 <abrusc93@students.rowan.edu>
2022-07-16 09:50:21 -0400

    Update script.js
    Fixed ScrollReveal elements so that they animate each time they are scrolled into the viewport and their origins alternate between top and bottom.

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 ScrollReveal().reveal('.reveal', { 
     delay: 200,
     distance: '50px',
+    origin: 'bottom',
+    reset: true,
+    afterReveal: revealFromTop
 });
+
+function revealFromTop (el) {
+    ScrollReveal().reveal(el, { 
+        delay: 200,
+        distance: '50px',
+        origin: 'top',
+        reset: true,
+        afterReveal: revealFromBottom
+    });
+}
+
+function revealFromBottom (el) {
+    ScrollReveal().reveal(el, { 
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: true,
+        afterReveal: revealFromTop
+    });
+}
