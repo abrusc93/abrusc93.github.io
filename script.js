382945e04e52ab73319ad31ff7c8079da66c0fed
abrusc93 <abrusc93@students.rowan.edu>
2022-07-16 10:09:08 -0400

    Update script.js
    Reverted back to having elements animate only once after page loads and they are scrolled into viewport.

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 ScrollReveal().reveal('.reveal', { 
     delay: 200,
-    distance: '50px',
-    origin: 'bottom',
-    reset: true,
-    afterReveal: revealFromTop
+    distance: '50px'
 });
-
-function revealFromTop (el) {
-    ScrollReveal().reveal(el, { 
-        delay: 200,
-        distance: '50px',
-        origin: 'top',
-        reset: true,
-        afterReveal: revealFromBottom
-    });
-}
-
-function revealFromBottom (el) {
-    ScrollReveal().reveal(el, { 
-        delay: 200,
-        distance: '50px',
-        origin: 'bottom',
-        reset: true,
-        afterReveal: revealFromTop
-    });
-}
